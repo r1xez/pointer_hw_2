cbabe091e6ef8a417bae1b0ee2823473e6d2fd54
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 23:08:59 +0300

    pointer hw1 ex1 was done

--- pointer_hw_2/main.cpp
+++ pointer_hw_2/main.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
-using namespace std;	
+using namespace std;
+
+
+bool contains(int* arr, int size, int value) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
+    int M, N;
 
+    cout << "Enter size of array M: ";
+    cin >> M;
 
-	return 0;
+    cout << "Enter size of array N: ";
+    cin >> N;
+
+   
+    int* A = new int[M];
+    int* B = new int[N];
+    int* C = new int[M];  
+    int sizeC = 0;  
+
+   
+    cout << "Enter elements of array M: ";
+    for (int i = 0; i < M; i++) {
+        cin >> A[i];
+    }
+
+   
+    cout << "Enter elements of array N: ";
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+
+   
+    for (int i = 0; i < M; i++) {
+        if (!contains(B, N, A[i]) && !contains(C, sizeC, A[i])) {
+            C[sizeC++] = A[i];
+        }
+    }
+
+    
+    cout << "Elements of array A not in array B, without duplicates: ";
+    for (int i = 0; i < sizeC; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+   
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    return 0;
 }
